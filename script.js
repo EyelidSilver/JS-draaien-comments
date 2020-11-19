b1097c03ae78b8e4eb78cf15673fea9b6f83c9c1
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2020-11-19 12:14:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hallo Winc Academy studenten");
+/*
+//Dit is een grote som (ik zie geen uitkomst in de console omdat comments door een machine negeert worden)
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division);
+*/
+// ik zie weer nieks omdat ik een deel van mijn code in een block comment ingestopt (alles binnen comments wordt negeert)
+
+
+
+
+
